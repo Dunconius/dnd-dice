fcd3b9e4c5dfd38e3a009fcc7fa67df251118566
Dunconius <duncanbean84@gmail.com>
2024-05-11 13:44:25 +1000

    docs: add docs to dice roll ufnctions

--- src/diceRolling.js
+++ src/diceRolling.js
@@ -1,11 +1,19 @@
 
-
+/**
+ * Generate a random number between 1 and a given size.
+ * @param [diceSize=6] -  represents size of die
+ * @returns A random number between 1 and given size
+ */
 function rollDice(diceSize = 6){
     let rollResult = Math.floor(Math.random() * diceSize) + 1
     return rollResult;
 }
 
-
+/**
+ * Generate a random number between 1 and a given size twice, and returns highest value.
+ * @param [diceSize=6] -  represents size of die
+ * @returns A random number between 1 and given size twice, then highest of those 2
+ */
 function rollWithAdvantage(diceSize = 6) {
     let result = {
         finalResult: null,
@@ -21,8 +29,29 @@ function rollWithAdvantage(diceSize = 6) {
     return result;
 }
 
+/**
+ * Generate a random number between 1 and a given size twice, and returns lowest value.
+ * @param [diceSize=6] -  represents size of die
+ * @returns A random number between 1 and given size twice, then lowest of those 2
+ */
+function rollWithDisadvantage(diceSize = 6) {
+    let result = {
+        finalResult: null,
+        rolls: []
+    };
+    
+    result.rolls = [
+        rollDice(diceSize),
+        rollDice(diceSize)
+    ];
+    
+    result.finalResult = Math.min(...result.rolls);
+    return result;
+}
+
 
 module.exports = {
     rollDice,
-    rollWithAdvantage
+    rollWithAdvantage,
+    rollWithDisadvantage
 }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
-const { rollDice, rollWithAdvantage } = require("./diceRolling");
+const { rollDice, rollWithAdvantage, rollWithDisadvantage } = require("./diceRolling");
 
 
 console.log(rollDice());
 console.log(rollDice(20));
 console.log(rollDice(12));
 console.log(rollWithAdvantage(20));
+console.log(rollWithDisadvantage(20));
